f5fb0d3487838617f79cde5b7b78cf89cfed1030
Gino <115049934+MrBluefish@users.noreply.github.com>
2023-06-02 21:04:53 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,11 +16,12 @@ public_users.get('/',function (req, res) {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+// Get book details based on ISBN task 2
+public_users.get("/isbn/:isbn", function (req, res) {
+    const bookISBN = req.params.isbn;
+    res.send(books[bookISBN]);
+  });
+  
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {

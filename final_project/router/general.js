b34ddf94666c96ffa08e0da3df336f8a760b861a
Gino <115049934+MrBluefish@users.noreply.github.com>
2023-06-03 03:52:17 -0400

    Update general.js
    redo task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,11 +10,12 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+// Get the book list available in the shop - tack 1
+public_users.get("/", function (req, res) {
+    res.send(JSON.stringify({ books }, null, 4))
+
+  });
+
 
 // Get book details based on ISBN - task 2
 public_users.get("/isbn/:isbn", function (req, res) {

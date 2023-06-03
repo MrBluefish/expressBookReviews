1c7386649e3753846de168b50269e1c01b132091
Gino <115049934+MrBluefish@users.noreply.github.com>
2023-06-03 04:03:22 -0400

    Update general.js
    username/password

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,9 +5,19 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ppublic_users.post("/register/",(req,res)=>{
+    const username = req.query.username;
+    const password = req.query.password;
+
+    if (username && password) {
+      if (!isValid(username)) {
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});
+      }
+    }
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop - tack 1

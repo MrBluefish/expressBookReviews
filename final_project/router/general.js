4894a7cfb84b9136b426f5ea5756df0cc46a145c
Gino <115049934+MrBluefish@users.noreply.github.com>
2023-06-04 19:23:25 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,12 +12,12 @@ public_users.post("/register/",(req,res)=>{
     if (username && password) {
       if (!isValid(username)) {
         users.push({"username":username,"password":password});
-        return res.status(200).json({message: "User successfully registred. Now you can login"});
+        return res.status(200).json({message: "User registered"});
       } else {
-        return res.status(404).json({message: "User already exists!"});
+        return res.status(404).json({message: "Existing user"});
       }
     }
-    return res.status(404).json({message: "Unable to register user."});
+    return res.status(404).json({message: "Registration unsuccessful"});
 });
 
 // Get the book list available in the shop - tack 1
